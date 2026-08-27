f40406038311f45333ecd496ac8718b5f284700c
beardlaws <officialbeardlaws@gmail.com>
2026-08-26 20:45:46 -0400

    Add Score Intelligence admin page

---
+++ src/app/admin/score-intelligence/page.tsx
@@ -0,0 +1,8 @@
+import AdminLayout from '@/components/layout/AdminLayout'
+import ScoreIntelligence from './ScoreIntelligence'
+
+export const revalidate=0
+
+export default function ScoreIntelligencePage(){
+  return <AdminLayout><ScoreIntelligence/></AdminLayout>
+}
