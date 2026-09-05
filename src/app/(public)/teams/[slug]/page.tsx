21fbff13f141adcc8182f839bd9cac61039fb928
beardlaws <officialbeardlaws@gmail.com>
2026-09-05 08:07:20 -0400

    Make public reads cache-friendly

--- src/app/(public)/teams/[slug]/page.tsx
+++ src/app/(public)/teams/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase/server'
+import { createPublicClient as createClient } from '@/lib/supabase/public'
 import { notFound } from 'next/navigation'
 import { Metadata } from 'next'
 import Link from 'next/link'
